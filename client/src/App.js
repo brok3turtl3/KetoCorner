59d6482c238ede88156a9c6cd677c00540eff6d6
Brok3Turtl3 <brok3turtl3@gmail.com>
2022-12-20 12:03:22 -0500

    Some inital express set up and front end navigation

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,29 @@
-
-
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { Container } from 'react-bootstrap';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Store from './screens/Store';
+import HomePage from './screens/HomePage';
+import ProductScreen from './screens/ProductScreen';
 
 const App = () => {
-  return (
-    <>
-      <h1>Welcome to Keto Corner!</h1>
-    </>
-  );
-}
+	return (
+		<Router>
+			<Header />
+			<main className='py-3'>
+				<Container>
+					<h1>Welcome to Keto Corner!</h1>
+					<Routes>
+						<Route path='/' element={<HomePage />} />
+						<Route path='/store' element={<Store />} />
+						<Route path='/product/:id' element={<ProductScreen />} />
+					</Routes>
+				</Container>
+			</main>
+			<Footer />
+		</Router>
+	);
+};
 
 export default App;
