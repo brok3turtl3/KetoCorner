e15074246d3cb309f93e031cce5c21ba3e5321fd
Brok3Turtl3 <brok3turtl3@gmail.com>
2023-01-29 11:34:43 -0500

    Heroku debugging

--- server/server.js
+++ server/server.js
@@ -28,14 +28,16 @@ app.get('/api/config/paypal', (req, res) =>
 
 const __dirname = path.resolve();
 
-if (
-	process.env.NODE_ENV === 'production' ||
-	process.env.NODE_ENV === 'staging'
-) {
-	app.use(express.static('client/build'));
-	app.get('/', (req, res) => {
-		res.sendFile(path.join(__dirname + '/client/build/index.html'));
-	});
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '/client/build')))
+
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  )
+} else {
+  app.get('/', (req, res) => {
+    res.send('API is running....')
+  })
 }
 
 app.use(notFound);
